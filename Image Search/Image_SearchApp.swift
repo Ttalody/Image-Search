3996a5fdc888a610ed3dbb7c5e44a755393723c7
Artur Akulich <akulich.artur@gmail.com>
2024-07-30 14:13:43 +0300

    Initial commint

---
+++ Image Search/Image_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_SearchApp.swift
+//  Image Search
+//
+//  Created by Артур on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Image_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
