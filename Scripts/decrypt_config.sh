211e03f10741e9efbbf1e166c7972192e3576537
Artur Akulich <akulich.artur@gmail.com>
2024-08-02 11:47:11 +0300

    Password removed

--- Scripts/decrypt_config.sh
+++ Scripts/decrypt_config.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f Config.plist ]; then
     echo "Decrypting Config.plist..."
-    openssl enc -aes-256-cbc -d -in Config.plist.enc -out Config.plist -k 12345
+    openssl enc -aes-256-cbc -d -in Config.plist.enc -out Config.plist -k YOUR_PASSWORD
 else
     echo "Config.plist already exists."
 fi
